01356b2e46acf8fd82375193de4a3c897dc4bd36
dylanjcm <dylanjanesky69@gmail.com>
2025-09-09 22:41:04 -0300

    added exercise 3 week 1 with stretch

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,29 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        int guess = -1;
-
-        while (guess != magicNumber)
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            if (magicNumber > guess)
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
             }
 
-        }                    
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
